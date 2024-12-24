d656e1fafe0663a5d7b48a8bd9b744e64e65fbf3
venessie <venplayz527@gmail.com>
2024-12-24 10:56:50 +0530

    [PYTHON]~ dec 24 2024

---
+++ print.py
@@ -0,0 +1,26 @@
+print("Extension: Python")
+
+print("PYTHON BASIC")
+
+print("Explorer")
+
+print("print.py")
+
+name = "Falie"
+age = 1
+
+print("Name is:", name + "Age is:",age)
+
+print("The 25 tables")
+print("25 x 1 = ", 25 * 1)
+print("25 x 2 = ", 25 * 2)
+print("25 x 3 = ", 25 * 3)
+print("25 x 4 = ", 25 * 4)
+print("25 x 5 = ", 25 * 5)
+print("25 x 6 = ", 25 * 6)
+print("25 x 7 = ", 25 * 7)
+print("25 x 8 = ", 25 * 8)
+print("25 x 9 = ", 25 * 9)
+print("25 x 10 = ", 25 * 10)
+print("25 x 11 = ", 25 * 11)
+print("25 x 12 = ", 25 * 12)
