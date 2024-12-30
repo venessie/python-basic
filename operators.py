e14931d712cf4209a027e45f0a52c422d820bdc9
venessie <venplayz527@gmail.com>
2024-12-30 10:52:17 +0530

    monday december 30 2024

---
+++ operators.py
@@ -0,0 +1,28 @@
+num1 = int(input ("Enter a number"))
+num2 = int(input ("Enter another number"))
+print("Addition",num1 + num2)
+print("Subtraction",num1 - num2)
+print("Multiplcation",num1 * num2)
+print("Division",num1 / num2)
+print("Floor Division",num1 // num2)
+print("Modulus",num1 % num2)
+print("Square",num1**2)
+print("Cube",num1**3)
+
+
+
+
+
+name = "Calie"
+age = 12
+is_student = True
+weight = 39.5
+
+print("Name:",name)
+print("Data type of name is..", type(name))
+print("Age:",age)
+print("Data type of age is..", type(age))
+print("Student?",is_student)
+print("Data type of is_student is..", type(is_student))
+print("Weight:",weight)
+print("Data type of weight is..", type(weight))
