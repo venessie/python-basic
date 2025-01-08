21b6170ebbe3afc094ae5745faeded958f31ca98
venessie <venplayz527@gmail.com>
2025-01-08 17:11:53 +0800

    wednesday januaray 8 2025

---
+++ list.py
@@ -0,0 +1,24 @@
+lst = ['Apple','Orange','Tangerine','Grapes','Pear','Blueberry']
+print("Length of List:",len(lst))
+print("First Element: Apple",lst[0])
+print("Last Element: Blueberry",lst[-1])
+lst.append('Raspberry')
+lst.remove('Blueberry')
+print(lst)
+lst.sort()
+print(lst)
+
+
+
+
+dict = {}
+dict = {1:'Venini',2:'Calili'}
+dict = {'Fruits':'Apple',2:[9,2,5]}
+dict = {'Name':'Kiana','Age':11,'Fruits':'Apple',2:[9,2,5]}
+print(dict['Name'])
+print(dict['Fruits'])
+print(dict['Age'])
+
+
+
+
---
+++ turtle.py
@@ -0,0 +1,6 @@
+import turtle
+turtle.title("Welcome to Turtle")
+
+b.forward(300)
+b.right(200)
+
