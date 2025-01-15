64f59108a6b4533130ecf0fb3fa0c41d6ad0eb0f
venessie <venplayz527@gmail.com>
2025-01-15 17:15:18 +0800

    e

---
+++ tuple.py
@@ -0,0 +1,31 @@
+tuple = ()
+print(tuple)
+tuple = (1,3,6,7,55,4,2,5)
+print(tuple)
+tuple = (8,5,"key","dot","/","?")
+print(tuple)
+tuple = ("word",[56,67,88,92,31,40],(7,6,5,4,3,2,1,8,9))
+print(tuple)
+tuple = ('c','o','d','i','n','g','a','l')
+print(tuple[0])
+print(tuple[3])
+print(tuple[6])
+print(tuple[-1])
+
+
+
+
+
+
+
+
+
+
+setc1 = {"20",  "40"}
+setc2 = {"40",   "30"}
+print("Original sets:")
+print(setc1)
+print(setc2)
+setc = setc1.intersection(setc2)
+print("\nIntersection of above sets:")
+print(setc)
